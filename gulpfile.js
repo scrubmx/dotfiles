612d69abcf6f03f4ff2e68e21314c613213167eb
Jorge González <scrub.mx@gmail.com>
2014-02-06 13:04:35 -0600

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,35 @@ var gulp      = require('gulp'),
     minifycss = require('gulp-minify-css'),
     notify    = require('gulp-notify'),
     jshint    = require('gulp-jshint'),
-    stylish   = require('jshint-stylish');
+    stylish   = require('jshint-stylish'),
+    uglify    = require('gulp-uglify'),
+    rename    = require('gulp-rename');
+
 
 // Minify and compile all LESS files
 gulp.task('less', function () {
     return gulp.src('public/css/**/*.less')
-      .pipe(less())
-      .pipe(minifycss({'keepBreaks':true}))
-      .pipe(gulp.dest('public/css/'))
-      .pipe(notify({ message: 'Compile less to css files completed.' }));
+        .pipe(less())
+        .pipe(minifycss({'keepBreaks':true}))
+        .pipe(gulp.dest('public/css/'))
+        .pipe(notify({ message: 'Compile less to css files completed.' }));
 });
 
+// Lint and minify javascript functions.js file
 gulp.task('lint', function () {
     return gulp.src('public/js/functions.js')
-      .pipe(jshint())
-      .pipe(jshint.reporter(stylish));
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish))
+        .pipe(jshint.reporter('fail'))
+        .pipe(uglify())
+        .pipe(rename('functions.min.js'))
+        .pipe(gulp.dest('public/js/'));
 });
 
-// Rerun the task when a file changes
+// Rerun the tasks when a file changes
 gulp.task('watch', function () {
-  gulp.watch('public/css/**/*.less', ['less']);
-  gulp.watch('public/js/functions.js', ['lint']);
+    gulp.watch('public/css/**/*.less', ['less']);
+    gulp.watch('public/js/functions.js', ['lint']);
 });
 
 // The default task (called when you run `gulp` from cli)
