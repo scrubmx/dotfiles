2d82c01049e9b7f7c4f7b78f6a0b834449fca7fc
Jorge González <scrub.mx@gmail.com>
2017-02-23 18:40:42 -0600

    Update upstream-sync.sh

--- upstream-sync.sh
+++ upstream-sync.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-# Sync a local fork of the laravel/framework repository
+# Sync fork repository with upstream
 
-# Change the output color to green
-# http://misc.flogisoft.com/bash/tip_colors_and_formatting#colors
+UPSTREAM="laravel/framework"
+declare -a BRANCHES=("master" "5.3" "5.4")
+
+# Set color green for status messages.
 function _print_status() {
     printf "\e[32m> %s\e[0m\n" "$@"
 }
 
 # Specify remote upstream repository that will be synced with the fork.
 if ! git config remote.upstream.url > /dev/null; then
-    git remote add upstream git@github.com:laravel/framework.git
+    git remote add upstream "git@github.com:$UPSTREAM.git"
 fi
 
 _print_status "Fetching the upstream remote branches..."
@@ -19,19 +21,12 @@ git fetch upstream
 
 _print_status "Syncing local branches with upstream..."
 
-# Sync master branch with the upstream repository
-git checkout master
-git merge upstream/master
-git push origin master
-
-# Sync 5.2 branch with the upstream repository
-git checkout 5.2
-git merge upstream/5.2
-git push origin 5.2
-
-# Sync 5.3 branch with the upstream repository
-git checkout 5.3
-git merge upstream/5.3
-git push origin 5.3
+for BRANCH in "${BRANCHES[@]}"; do
+    _print_status "Syncing $BRANCH branch..."
+    git branch -D $BRANCH
+    git checkout upstream/$BRANCH
+    git checkout -b $BRANCH
+    git push -f origin $BRANCH
+done
 
 _print_status "Your fork is now up to date!"
