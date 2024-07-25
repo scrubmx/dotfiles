17ca6692acf6596ef79d239356b96de3fd040a11
Jorge González <scrub.mx@gmail.com>
2024-07-24 23:04:23 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,50 +1,66 @@
 #!/bin/sh
 
-# Install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install Oh My Zsh
+# https://ohmyz.sh/#install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Get our custom zsh-theme
+wget -O ~/.oh-my-zsh/themes/custom.zsh-theme https://raw.githubusercontent.com/scrubmx/dotfiles/master/.oh-my-zsh/themes/custom.zsh-theme
+
+# Install Homebrew
+# https://brew.sh/#install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew analytics off
+
 brew update
 
-brew install fortune
-brew install cowsay
-brew install lolcat
-brew install lsd
-brew install bat
-brew install fzf
-brew install grep
-brew install hyperfine
-brew install jq
 brew install wget
-brew install terminal-notifier
-brew install tty-clock
-brew install tmux
-brew install tree
-brew install httpie
-brew install diff-so-fancy
-brew install git
-brew install neofetch
-brew install neovim
-brew install nginx
+brew install bat
+brew install lsd
+brew install grep
 brew install ripgrep
-brew install spotify-tui
-brew install sqlite
+brew install hyperfine
+brew install diff-so-fancy
+brew install tty-clock
+brew install jq
+brew install fzf
+brew install neovim
+brew install tmux
+
+# brew install fortune
+# brew install cowsay
+# brew install lolcat
+# brew install spotify-tui
+# brew install terminal-notifier
+
+# brew install elixir
 brew install node
-brew install yabai
-brew install yarn
 brew install php@8.3
-brew install mariadb
-brew install postgresql@16
 brew install composer
-brew install elixir
+brew install psysh
+brew install nginx
+
+brew install sqlite
+# brew install mariadb
+# brew install mysql@8.0
+# brew install postgresql@16
+
+brew install yabai
+brew install koekeishiya/formulae/skhd
+# yabi --start-service
+# skhd --start-service
 
 # Install fonts
-brew tap caskroom/fonts
-brew cask install font-hack
+# https://www.nerdfonts.com/font-downloads
+brew tap homebrew/cask-fonts
 
-# Install timer https://github.com/caarlos0/timer
-brew tap caarlos0/homebrew-tap
-brew install timer
+# https://github.com/githubnext/monaspace
+brew install font-monaspace
+# https://github.com/tonsky/FiraCode
+brew install font-fira-code
+# https://github.com/source-foundry/Hack
+brew install font-hack
 
 # Install applications
 # brew tap homebrew/cask
@@ -54,49 +70,46 @@ brew install --cask 1password
 brew install --cask alfred
 brew install --cask brave-browser
 brew install --cask coderunner
-brew install --cask daisydisk
-brew install --cask discord
-brew install --cask dropbox
-brew install --cask droplr
-brew install --cask figma
 brew install --cask github
-# brew install --cask google-chrome
 brew install --cask insomnia
-brew install --cask iterm2
-brew install --cask jumpshare
-brew install --cask karabiner-elementsq
-brew install --cask moom
-brew install --cask mysqlworkbench
-brew install --cask nordvpn
-brew install --cask phpstorm
+brew install --cask json-viewer
+brew install --cask karabiner-elements
+brew install --cask kitty
+# brew install --cask moom
 brew install --cask qlmarkdown
 brew install --cask slack
 brew install --cask spotify
 brew install --cask tableplus
 brew install --cask transmission
-brew install --cask transmit
 brew install --cask vitamin-r
-brew install --cask visual-studio-code
 brew install --cask vlc
 brew install --cask zoom
+
+# brew install --cask daisydisk
+# brew install --cask discord
+# brew install --cask dropbox
+# brew install --cask droplr
+# brew install --cask figma
+# brew install --cask google-chrome
+# brew install --cask httpie
+# brew install --cask iterm2
+# brew install --cask jumpshare
+# brew install --cask mysqlworkbench
+# brew install --cask nordvpn
+# brew install --cask phpstorm
+# brew install --cask transmit
+# brew install --cask visual-studio-code
 
 # Install software from the Mac App Store
 brew install mas
 
-mas install 937984704 # Amphetamine
-mas install 904280696 # Things
+# mas install 937984704 # Amphetamine
+# mas install 904280696 # Things
 mas install 1091189122 # Bear
-mas install 605732865 # RSS Bot
+# mas install 605732865 # RSS Bot
 
 # Remove stale lock files and outdated downloads
 brew cleanup
-
-# Install oh-my-zsh
-# https://github.com/ohmyzsh/ohmyzsh#basic-installation
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-
-# Get our custom theme
-wget -O ~/.oh-my-zsh/themes/custom.zsh-theme https://raw.githubusercontent.com/scrubmx/dotfiles/master/.oh-my-zsh/themes/custom.zsh-theme
 
 # Install global composer packages
 composer global require laravel/installer
